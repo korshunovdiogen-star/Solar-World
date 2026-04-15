cf756dd7a51ec46f99c362869c74a9a7dff482be
korshunovdiogen-star <korshunovdiogen@mail.ru>
2026-04-15 15:59:00 +0700

    Добавление адаптивного дизайна

--- solarWorld/settings.py
+++ solarWorld/settings.py
@@ -16,11 +16,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# ALLOWED_HOSTS = ['LorAdmin.pythonanywhere.com']
+ALLOWED_HOSTS = ['LorAdmin.pythonanywhere.com']
 
 
 # Application definition
